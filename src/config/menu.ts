32d0be7cdf9f5cc00f908fbf9657878056d03aee
nichom01 <martin@neversoft.co.uk>
2025-10-03 14:33:29 +0100

    Initial commit (from base-ui template)

---
+++ src/config/menu.ts
@@ -0,0 +1,51 @@
+import {
+    CircleAlert,
+    Files,
+    Gauge,
+    LucideIcon
+} from 'lucide-react'
+
+type MenuItemType = {
+    title: string
+    url: string
+    external?: string
+    icon?: LucideIcon
+    items?: MenuItemType[]
+}
+type MenuType = MenuItemType[]
+
+export const mainMenu: MenuType = [
+    {
+        title: 'Dashboard',
+        url: '/',
+        icon: Gauge
+    },
+    {
+        title: 'Pages',
+        url: '/pages',
+        icon: Files,
+        items: [
+            {
+                title: 'Sample Page',
+                url: '/pages/sample',
+            },
+            {
+                title: 'Coming Soon',
+                url: '/pages/feature',
+            },
+            {
+                title: 'Graph',
+                url: '/pages/chart',
+            },
+            {
+                title: 'Sign In',
+                url: '/pages/login',
+            },
+        ]
+    },
+    {
+        title: 'Error',
+        url: '/404',
+        icon: CircleAlert,
+    },
+]
